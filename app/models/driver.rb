8674db3bef1b43196bfa702fda0199fb7ad19874
Patrick <patrickwiechert@yahoo.com>
2016-05-03 23:19:05 -0500

    added more testing

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,5 @@
 class Driver < ActiveRecord::Base
+  validates :name, presence: true
+  validates :team, presence: true
+  validates :motor, presence: true
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DriverTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "Should be valid by default" do
+    @driver = drivers(:one)
+    assert(@driver.valid?, "Should be valid at creation")
+  end
+
+  test "Should be invalid without a name" do
+    @driver = drivers(:one)
+    @driver.name = nil
+    refute(@driver.valid?, "Should be invalid without a name")
   end
 end
