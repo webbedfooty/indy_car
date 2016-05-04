b2daef7139b29076253eb26d1592c699b461140a
Patrick <patrickwiechert@yahoo.com>
2016-05-03 21:49:28 -0500

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+
   get "/about" => 'static_pages#about', as: :about_page
+
+  get "/hello-form" => 'greetings#new', as: :new_greeting
+  get "/hello" => 'grettings#show', as: :greeting
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
